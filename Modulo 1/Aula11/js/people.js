82f3c7219a249a9624e00b7bc01a2d9c874ab1e9
Gustavo Correa de Lyra <gustavo.c.lyra@live.com>
2020-07-13 17:45:46 -0300

    Aula 11 Manipulação Array

---
+++ Modulo 1/Aula11/js/people.js
@@ -0,0 +1,86 @@
+const people = {
+  results: [
+    {
+      gender: 'female',
+      name: { title: 'Mrs', first: 'Expedita', last: 'Cavalcanti' },
+      location: {
+        street: { number: 2884, name: 'Rua São João ' },
+        city: 'Camaragibe',
+        state: 'Roraima',
+        country: 'Brazil',
+        postcode: 40435,
+        coordinates: { latitude: '-74.2290', longitude: '71.3214' },
+        timezone: {
+          offset: '+10:00',
+          description: 'Eastern Australia, Guam, Vladivostok',
+        },
+      },
+      email: 'expedita.cavalcanti@example.com',
+      login: {
+        uuid: '526f4c2d-51c4-4143-9997-8118abcc5b55',
+        username: 'heavyleopard202',
+        password: 'member',
+        salt: 'gUqgsmZK',
+        md5: '172db319c795d80141ccadefe727f27c',
+        sha1: '96b3cefd632298f94f6ee003c90343b59b9c68c2',
+        sha256:
+          'cc18ca3e457dbb330253f65cb147b6f2235a369004d034236450d86671f13aee',
+      },
+      dob: {
+        date: '1951-01-24T01:59:44.147Z',
+        age: 69,
+      },
+      registered: {
+        date: '2011-08-13T16:17:21.959Z',
+        age: 9,
+      },
+      phone: '(78) 7363-3202',
+      cell: '(03) 9339-5661',
+      id: {
+        name: '',
+        value: null,
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/women/61.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/women/61.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/61.jpg',
+      },
+      nat: 'BR',
+    },
+    {
+      gender: 'female',
+      name: { title: 'Mrs', first: 'Fatima', last: 'Ramirez' },
+      location: {
+        street: { number: 2587, name: 'Calle de Toledo' },
+        city: 'Ciudad Real',
+        state: 'Castilla la Mancha',
+        country: 'Spain',
+        postcode: 74413,
+        coordinates: { latitude: '-23.8286', longitude: '128.8572' },
+        timezone: { offset: '-10:00', description: 'Hawaii' },
+      },
+      email: 'fatima.ramirez@example.com',
+      login: {
+        uuid: '6ba14a34-b34f-48f9-a728-60ee1fe49dfa',
+        username: 'lazygorilla120',
+        password: 'bummer',
+        salt: 'nq8G8JiC',
+        md5: 'c3a3d6ec87ca4297c727cdc792dfb8ba',
+        sha1: '6c13fb1b6f47c4cf3233b39844d191e5aae033c1',
+        sha256:
+          '36b796d5ed15ea72bf49341f3b279a382c61fb6290fc15aabe80ae9f0910a2d6',
+      },
+      dob: { date: '1994-10-26T17:02:10.883Z', age: 26 },
+      registered: { date: '2018-07-17T03:50:42.871Z', age: 2 },
+      phone: '999-358-962',
+      cell: '659-687-206',
+      id: { name: 'DNI', value: '24601522-B' },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/women/7.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/women/7.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
+      },
+      nat: 'ES',
+    },
+  ],
+};
---
+++ Modulo 1/Aula11/js/scrypt.js
@@ -0,0 +1,81 @@
+window.addEventListener('load', () => {
+  doMap();
+  doFilter();
+  doForEach();
+  doReduce();
+  doFind();
+  doSome();
+  doEvery();
+  doSort();
+});
+
+function doMap() {
+  const nameEmailArray = people.results.map((person) => {
+    return {
+      name: person.name,
+      email: person.email,
+    };
+  });
+  console.log(nameEmailArray);
+  return nameEmailArray;
+}
+
+function doFilter() {
+  const olderThan18 = people.results.filter((person) => {
+    return person.dob.age >= 18;
+  });
+  console.log(olderThan18);
+}
+
+function doForEach() {
+  const mappedPeople = doMap();
+
+  mappedPeople.forEach((person) => {
+    person.nameSize =
+      person.name.title.length +
+      person.name.first.length +
+      person.name.last.length;
+  });
+
+  console.log(mappedPeople);
+}
+
+function doReduce() {
+  const totalAges = people.results.reduce((accumulator, current) => {
+    return accumulator + current.dob.age;
+  }, 0);
+  console.log(totalAges);
+}
+
+function doFind() {
+  const found = people.results.find((person) => {
+    return person.dob.age >= 60;
+  });
+  console.log(found.name.first);
+}
+
+function doSome() {
+  const found = people.results.some((person) => {
+    return person.location.state === 'Roraima';
+  });
+  console.log(found);
+}
+
+function doEvery() {
+  const every = people.results.every((person) => {
+    return person.nat === 'US';
+  });
+  console.log(every);
+}
+
+function doSort() {
+  const sort = people.results
+    .map((person) => {
+      return person.name.first;
+    })
+    .filter((person) => {
+      return person.startsWith('F');
+    })
+    .sort();
+  console.log(sort);
+}
