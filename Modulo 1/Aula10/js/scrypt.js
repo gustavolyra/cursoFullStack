141ecda88eb53eb1a1f25f12aca48b7c0a5c3d4f
Gustavo Correa de Lyra <gustavo.c.lyra@live.com>
2020-07-13 00:59:19 -0300

    aula 10 p2

--- Modulo 1/Aula10/js/scrypt.js
+++ Modulo 1/Aula10/js/scrypt.js
@@ -3,8 +3,18 @@
 window.addEventListener('load', start);
 
 function start() {
-  withVar();
-  withLet();
+  //withVar();
+  //withLet();
+  console.log(sum(1, 2));
+  console.log(sum2(1, 2));
+  console.log(sum3(1, 2));
+  console.log(sum4(1, 2));
+  templateLiteral();
+
+  //função com valor default
+  console.log(sum5(1, 2));
+  console.log(sum5(1));
+  console.log(sum5());
 }
 
 function withVar() {
@@ -21,6 +31,32 @@ function withLet() {
     console.log('var' + i);
   }
   //com let gera um erro ao tentar acessar i fora do for
-  i = 20;
-  console.log(i);
+  // i = 20;
+  // console.log(i);
 }
+
+//Formas de Declarar funções
+function sum(a, b) {
+  return a + b;
+}
+
+const sum2 = function (a, b) {
+  return a + b;
+};
+
+const sum3 = (a, b) => {
+  return a + b;
+};
+const sum4 = (a, b) => a + b;
+
+const sum5 = (a = 10, b = 5) => a + b;
+
+const templateLiteral = () => {
+  //template literals
+  const name = 'Gustavo';
+  const surName = 'Lyra';
+  const text1 = 'Meu nome é ' + name + ' ' + surName;
+  console.log(text1);
+  const text2 = `Meu nome é ${name} ${surName}`;
+  console.log(text2);
+};
